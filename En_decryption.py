2579e0e01c86129eeb3e5fb8a18e4c38c69f60b1
seema adhikari <seemateach23@gmail.com>
2025-05-15 01:31:35 +0545

    Second commit

--- En_decryption.py
+++ En_decryption.py
@@ -13,8 +13,31 @@ def encrypt_caesar(plaintext, key):
 
 
     return ciphertext
+
+def decrypt_caesar(ciphertext, key):
+    plaintext = ""
+    for char in ciphertext:
+        if char.isalpha():
+            # seeing its uppercase/lowercase
+            upcase = 65 if char.isupper() else 97
+            decrypted_char = chr((ord(char)-upcase - key+26)%26 + upcase)
+            plaintext += decrypted_char
+
+
+        else:
+            plaintext +=char # if char is not an alphabet(punctuation, space) don't change
+
+
+    return plaintext
+
+
 message = "Hello, world!"
 key = 3
+
 encrypted_message = encrypt_caesar(message, key)
+decrypted_message = decrypt_caesar(encrypted_message, key)
+
+print("plaintext:", message)
 print("Encrypted:", encrypted_message)
+print("decrypted:", decrypted_message)
 
